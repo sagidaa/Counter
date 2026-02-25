eca55f97df37dddd7259da899a8588da7fc77d2a
Sagida <sagida.karzhaubayeva@gmail.com>
2026-02-25 19:54:07 +0500

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Sagida on 25.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
