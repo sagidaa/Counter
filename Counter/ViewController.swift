7c56a2e3e637532424d22e5c180356d4fda1b565
Sagida <sagida.karzhaubayeva@gmail.com>
2026-03-12 17:55:57 +0500

    Исправлены комментарии ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,48 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var incrementButton: UIButton!
-    @IBOutlet weak var decrementButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    @IBOutlet weak var historyTextView: UITextView!
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var incrementButton: UIButton!
+    @IBOutlet private weak var decrementButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
+    @IBOutlet private weak var historyTextView: UITextView!
     
     private var counterNumber = 0
     
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureUI()
+    }
+    
+    private func configureUI() {
         updateCounterLabel()
-        
         historyTextView.text = "История изменений:\n"
-    }
-
-    @IBAction func incrementButtonTapped(_ sender: Any) {
-        counterNumber += 1
-        updateCounterLabel()
-        updateHistory("значение изменено на +1")
-        print("Increment button was tapped. Value: \(counterNumber)")
-    }
-    
-    @IBAction func decrementButtonTapped(_ sender: Any) {
-        if counterNumber > 0 {
-            counterNumber -= 1
-            updateHistory("значение изменено на -1")
-        } else {
-            updateHistory("попытка уменьшить значение счётчика ниже 0")
-        }
-        updateCounterLabel()
-        print("Decrement button was tapped. Value: \(counterNumber)")
-    }
-    
-    
-    @IBAction func resetButtonTapped(_ sender: Any) {
-        counterNumber = 0
-        updateCounterLabel()
-        updateHistory("значение сброшено")
-        print("Reset button was tapped. Value: \(counterNumber)")
     }
     
     private func updateCounterLabel() {
@@ -56,9 +39,39 @@ class ViewController: UIViewController {
     }
     
     private func updateHistory(_ text: String) {
-        let time = DateFormatter.localizedString(from: .now, dateStyle: .short, timeStyle: .short)
+        let time = dateFormatter.string(from: Date())
         historyTextView.text += "\(time): \(text)\n"
+        scrollHistoryToBottom()
     }
     
+    private func scrollHistoryToBottom() {
+        let bottom = NSRange(
+            location: historyTextView.text.count - 1,
+            length: 1
+        )
+        historyTextView.scrollRangeToVisible(bottom)
+    }
+    
+    @IBAction private func incrementButtonTapped(_ sender: Any) {
+        counterNumber += 1
+        updateCounterLabel()
+        updateHistory("значение изменено на +1")
+    }
+    
+    @IBAction private func decrementButtonTapped(_ sender: Any) {
+        if counterNumber > 0 {
+            counterNumber -= 1
+            updateHistory("значение изменено на -1")
+        } else {
+            updateHistory("попытка уменьшить значение счётчика ниже 0")
+        }
+        updateCounterLabel()
+    }
+    
+    @IBAction private func resetButtonTapped(_ sender: Any) {
+        counterNumber = 0
+        updateCounterLabel()
+        updateHistory("значение сброшено")
+    }
 }
 
